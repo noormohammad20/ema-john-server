7d43c0990fb7b28af35bfb78a0926054515c59dc
noormohammad20 <mdsaim204@gmail.com>
2022-04-23 20:58:11 +0600

    express initilse done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const express = require('express')
+const cors = require('cors')
+const app = express()
+
+const port = process.env.PORT || 5000
+
+// middleware
+app.use(cors())
+app.use(express())
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.listen(port, () => {
+    console.log('form server', port)
+})
